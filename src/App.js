5a6e454daf5bc68a13ea7aa75d1f97310f3511f2
mukarrom21 <mukarrom21@gmail.com>
2022-05-22 11:19:53 +0600

    carousel and active link added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
 import Navbar from "./pages/Shared/Navbar";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="">
       <Navbar></Navbar>
-      
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/pages/Shared/Navbar.js
+++ src/pages/Shared/Navbar.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import CustomLink from "../../CustomLink";
 
 const Navbar = () => {
+  const menu = (
+    <>
+      <li>
+        <CustomLink to={"/home"}>Home</CustomLink>
+      </li>
+      <li>
+        <CustomLink to={"/blog"}>Blog</CustomLink>
+      </li>
+      <li>
+        <CustomLink to={"/about"}>About</CustomLink>
+      </li>
+    </>
+  );
   return (
     <nav>
       <div class="navbar bg-light-1">
@@ -26,69 +41,13 @@ const Navbar = () => {
               tabindex="0"
               class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li tabindex="0">
-                <a class="justify-between">
-                  Parent
-                  <svg
-                    class="fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                  </svg>
-                </a>
-                <ul class="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {menu}
             </ul>
           </div>
           <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
         </div>
         <div class="navbar-center hidden lg:flex">
-          <ul class="menu menu-horizontal p-0">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li tabindex="0">
-              <a>
-                Parent
-                <svg
-                  class="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                </svg>
-              </a>
-              <ul class="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
+          <ul class="menu menu-horizontal p-0">{menu}</ul>
         </div>
         <div class="navbar-end">
           <a class="btn btn-primary">Get started</a>
