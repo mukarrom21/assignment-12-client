b96f5b72d72423481071516a281b181534e0fe41
Mukarrom <mukarrom21@yahoo.com>
2022-05-28 23:22:26 +0600

    git pull request

--- src/pages/Products/Purchase .js
+++ src/pages/Products/Purchase .js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const Purchase  = () => {
-    return (
-        <div>
-            This is Purchase page.
-        </div>
-    );
+const Purchase = () => {
+  return (
+    <div class="form-control w-full max-w-xs">
+      <label class="label">
+        <span class="label-text">What is your name?</span>
+      </label>
+      <input
+        type="text"
+        placeholder="Type here"
+        class="input input-bordered w-full max-w-xs"
+      />
+      <label class="label">
+        <span class="label-text-alt">Alt label</span>
+      </label>
+    </div>
+  );
 };
 
-export default Purchase ;
+export default Purchase;
